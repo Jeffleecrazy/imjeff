9da911c305ee60aa26d4f0f23d853068d0564241
Jeffleecrazy <104436443+Jeffleecrazy@users.noreply.github.com>
2022-04-26 21:46:51 +0800

    Update Ex1-2.cpp

--- Ex1-2.cpp
+++ Ex1-2.cpp
@@ -1,5 +1,7 @@
 #include<stdio.h>
 int main (void) {
      printf(" hello Selena "); 
-     printf(" my height is %d m %d cm \n",1,80);
+     printf(" my height is << 1 << "m " << 79 << "cm" \n",1,80);
+     return 0;
 }
+
